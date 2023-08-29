b185e9480a099ebc1b813efc2391f2beaf807e52
Décio Francis <francisdasilva@gmail.com>
2023-08-29 16:37:18 -0300

    treinando criacao de formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,93 @@
 import React from 'react';
-import Produto from './Produto';
+
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password',
+  },
+  {
+    id: 'cep',
+    label: 'Cep',
+    type: 'text',
+  },
+  {
+    id: 'rua',
+    label: 'Rua',
+    type: 'text',
+  },
+  {
+    id: 'numero',
+    label: 'Numero',
+    type: 'number',
+  },
+  {
+    id: 'bairro',
+    label: 'Bairro',
+    type: 'text',
+  },
+  {
+    id: 'cidade',
+    label: 'Cidade',
+    type: 'text',
+  },
+  {
+    id: 'estado',
+    label: 'Estado',
+    type: 'text',
+  },
+]
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }, {}),
+  );
+  const [response, setResponse] = React.useState(null);
 
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem('produto');
-    if (produtoLocal !== 'null') setProduto(produtoLocal);
-  }, []);
+  function handleChange({target}) {
+    const {id, value} = target;
+    setForm({ ...form, [id]: value });
+  }
 
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem('produto', produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    })
   }
 
   return (
-    <div>
-      <h1>Preferência: {produto}</h1>
-      <button style={{ marginRight: '1rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-      <Produto produto={produto} />
-    </div>
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input type={type} id={id} value={form[id]} onChange={handleChange}/>
+        </div>
+      ))}
+      {response && response.ok && <p>Formulario Enviado</p>}
+      <button>Enviar</button>
+    </form>
   );
 };
 
