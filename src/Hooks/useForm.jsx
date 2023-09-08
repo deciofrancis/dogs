9cdcfc6dc5d702efb8a2cef6fa2783faf5b66b03
Décio Francis <francisdasilva@gmail.com>
2023-09-08 13:26:13 -0300

    desenvolvendo criar login

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -4,6 +4,10 @@ const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email valido',
+    },
+    password: {
+      regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+      message: 'A senha precisa ter 1 caracter maiusculo, 1 minusculo e 1 digito. Com no minimo 8 caracteres.',
     }
 }
 
