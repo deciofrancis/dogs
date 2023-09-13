d7470ec718b33aa6a1bb22f31c3a15d3ff2e268a
Décio Francis <francisdasilva@gmail.com>
2023-09-12 21:17:58 -0300

    criando estrutura do componente user

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
+  return <section>
+    <UserHeader />
+    <Routes>
+      <Route patch="/" element={<Feed />} />
+      <Route patch="postar" element={<UserPhotoPost />} />
+      <Route patch="estatisticas" element={<UserStats />} />
+    </Routes>
+  </section>
 }
 
 export default User;
