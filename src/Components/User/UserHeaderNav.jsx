b627474c38f82d4124e9ff488ec2371b1d06f9f0
Décio Francis <francisdasilva@gmail.com>
2023-09-14 21:16:00 -0300

    adicionando icons menu mobile header

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import { ReactComponent as MinhasFots } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFotos } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
-  const {userLogout} = React.useContext(UserContext);
+  const [mobile, setMobile] = React.useState(null);
+  const { userLogout } = React.useContext(UserContext);
   
   return (
-    <nav>
-        <NavLink to="/conta">Minhas Fotos</NavLink>
-        <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
-        <NavLink to="/conta/postar">Adicionar Foto</NavLink>
-        <button onClick={userLogout}>Sair</button>
+    <nav className={styles.nav}>
+        <NavLink to="/conta">
+          <MinhasFots />
+          { mobile && 'Minhas Fotos' }
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          { mobile && 'Estatisticas' }
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFotos />
+          { mobile && 'Adicionar Foto' }
+        </NavLink>
+        <button onClick={userLogout}> 
+          <Sair />
+          { mobile && 'Sair' }
+        </button>
     </nav>
   );
 }
