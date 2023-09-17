19ff138aa8abf0e5caad3c54b5ae8a41c72509b2
Décio Francis <francisdasilva@gmail.com>
2023-09-16 21:46:12 -0300

    terminando desenvolvimento user header e corrigindo erro

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import UserHeaderNav from './UserHeaderNav';
+import { useLocation } from 'react-router-dom';
+
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    const { pathname } = location;
+    switch(pathname) {
+      case '/conta/postar':
+        setTitle('Poste sua foto');
+        break
+      case '/conta/estatisticas':
+        setTitle('Estatisticas');
+        break;
+      default:
+        setTitle('Minha conta')
+    }
+  }, [location]);
+
   return (
-    <header></header>
-  )
-}
+    <header className={StyleSheet.header}>
+      <h1 className="title">{title}</h1>
+      <UserHeaderNav />
+    </header>
+  );
+};
 
 export default UserHeader;
