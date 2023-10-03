db0c4f1fe15c20e5ddf7e1d1a492a49f2468b8be
Décio Francis <francisdasilva@gmail.com>
2023-10-02 22:34:17 -0300

    start developer feed photos

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 
 const FeedPhotos = () => {
-  return (
-    <div>FeedPhotos</div>
-  )
+    const {data, loading, error, request} = useFetch();
+
+    React.useEffect(() => {
+        async function fetchPhotos() {
+            const {url, options} = PHOTOS_GET({ page: 1, total: 6, user:0 });
+            const {response, json} = await request(url, options);
+        }
+        fetchPhotos();
+    }, [request]);
+  
+    if (error) return <Error error={error} />;
+    if (loading) return <Loading />;
+    if (data) 
+        return (
+            <ul>
+                {data.map(photo => (
+                    <FeedPhotosItem key={photo.id} photo={photo} />
+                ))}
+            </ul>
+        )
+    else return null;
 }
 
 export default FeedPhotos;
