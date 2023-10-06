688981414e4a10279b79c6952022d626205b9653
Décio Francis <francisdasilva@gmail.com>
2023-10-05 21:19:54 -0300

    styling feed modal

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import styles from './FeedPhotoItem.module.css';
 
 const FeedPhotosItem = ({ photo, setModalPhoto}) => {
   function handleClick() {
