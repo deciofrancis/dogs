78225f68ea6a7fd5425c19772fdbb56f7150422e
Décio Francis <francisdasilva@gmail.com>
2023-10-27 23:54:54 -0300

    feat add graph Pie from the lib victory

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
     const [graph, setGraph] = React.useState([]);
     const [total, setTotal] = React.useState(0);
 
     React.useEffect(() => {
+        const graphData = data.map(item => {
+            return {
+                x: item.title,
+                y: Number(item.acessos)
+            }
+        })
         setTotal(
             data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b , 0),
-        )
+        );
+        setGraph(graphData);
     }, [data]);
 
     return (
         <section className={`$styles.graph`}>
             <div className={styles.total}>
                 <p>Acessos: {total}</p>
+            </div>
+            <div>
+                <VictoryPie 
+                    data={[graph]} 
+                    innerRadius={50}
+                    padding={{ top: 20, bottom: 2, left: 80, right: 80 }}
+                    style={{
+                        data: {
+                            fillOpacity: .9,
+                            stroke: '#fff',
+                            stroKeWidth: 2,
+                        },
+                        labels: {
+                            fontSize: 14,
+                            fill: '#333'
+                        }
+                    }}
+                />
             </div>
         </section>
     )
