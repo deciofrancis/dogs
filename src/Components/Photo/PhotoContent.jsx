7db5414f36cddab9bf7605992d5db93b1c7fc86a
Décio Francis <francisdasilva@gmail.com>
2023-11-06 21:32:11 -0300

    fix: errors

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -21,7 +21,7 @@ const PhotoContent = ({ data, single }) => {
                             <PhotoDelete id={photo.id} />
                         ) : (
                             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-                        )};
+                        )}
                         <span className={styles.visualizacoes}>{photo.acessos}</span>
                     </p>
                     <h1 className="title">
