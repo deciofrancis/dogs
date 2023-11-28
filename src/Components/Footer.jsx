72d994595e731e04374ff434b975595475f3fc64
Décio Francis <francisdasilva@gmail.com>
2023-11-27 23:39:49 -0300

    chore: eslint errors footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from  './Footer.module.css';
+import styles from './Footer.module.css';
 import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <Dogs />
-            <p>Dogs. Aguns direitos reservados.</p>
+            <p>Dogs. Alguns direitos reservados.</p>
         </footer>
     );
 };
